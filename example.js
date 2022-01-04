51a2b6f043b39481e5c510c7b9f0109499f06ce6
Matt Travi <programmer@travi.org>
2022-01-04 17:16:31 -0600

    docs(example): populated required files on the stubbed filesystem

--- example.js
+++ example.js
@@ -1,13 +1,21 @@
 // #### Import
-// remark-usage-ignore-next
+// remark-usage-ignore-next 3
+import {promises as fs} from 'fs';
+import {resolve} from 'path';
 import stubbedFs from 'mock-fs';
 import {scaffold} from './lib/index.cjs';
 
-// remark-usage-ignore-next
-stubbedFs();
-
 // #### Execute
 
+// remark-usage-ignore-next 7
 (async () => {
+  stubbedFs({
+    node_modules: stubbedFs.load(resolve(__dirname, 'node_modules')),
+    templates: {'nuxt.config.js': await fs.readFile(resolve(__dirname, 'templates', 'nuxt.config.js'))},
+    'nuxt.config.js': await fs.readFile(resolve(__dirname, 'templates', 'nuxt.config.js'))
+  });
+
   await scaffold({projectRoot: process.cwd()});
+  // remark-usage-ignore-next 2
+  stubbedFs.restore();
 })();
